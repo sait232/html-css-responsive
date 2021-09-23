86ac47206eadfb81a5f1a7b41f2fc6e099ec7888
sait232 <85009810+sait232@users.noreply.github.com>
2021-09-23 09:16:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// function multiples(){
+//     for(let i=1;i<=20;i++){ 
+
+//      if(i%2==0&&i%3!=0&&i%5!==0){
+//          console.log(`hello :${i}`);
+//      }
+// else  if(i%3==0){
+//          console.log(`hello world :${i}`);
+//      }
+//   else   if(i%5==0){
+//          console.log(`javascript :${i}`);
+//      }
+//     }
+//   }
+//   multiples();
+
+
+//interview code
+ //   let colors=["red"];
+//     for(let i=0;i<colors.length;i++){
+//     console.log(colors[i].length);
+//     }
+
+
+//     console.log(false=="0");
+//     console.log(false==="0");
+
+// if(NaN){
+// console.log("hello");
+// }
+// else{
+//   console.log("world");
+// } -->
+
+
+// abhi task
+ // if(i%2==0){
+
+    //     console.log(`hello :${i}`);
+    // }
+//             if(i%3==0){
+
+//                console.log(`hello world :${i}`);
+//             }
+//           if(i%5==0){
+
+//         console.log(`welcome javascript :${i}`);
+// } -->
+
+
+    
